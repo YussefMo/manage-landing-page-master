9ba4c8566ca0bd929feec76b19234ffeb8fd4720
Youssef Mohammed <49940573+YussefMo@users.noreply.github.com>
2024-12-03 19:20:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,35 @@ document.querySelector('.bars').addEventListener('click',()=>
     document.querySelector('.bars').classList.toggle('change');
 });
 
+// toggel script
+let body= document.body
+document.querySelector('.toggle-cont').addEventListener('click',()=>
+{
+    body.classList.toggle('dark');
+    document.querySelector('.circle').classList.toggle('chng');
+    document.querySelector('.toggel').classList.toggle('chng');
+    let x = document.querySelector('.circle')
+    if(x.classList.contains('chng'))
+    {
+        document.querySelector('.fa-sun').classList.remove('active');
+        document.querySelector('.fa-moon').classList.add('active');
+    }
+    else
+    {
+        document.querySelector('.fa-sun').classList.add('active');
+        document.querySelector('.fa-moon').classList.remove('active');
+    }
+    if(body.classList.contains('dark'))
+    {
+        document.querySelector('.white-lo').classList.remove('hide')
+        document.querySelector('.dark-lo').classList.add('hide')
+    }
+    else
+    {
+        document.querySelector('.white-lo').classList.add('hide')
+        document.querySelector('.dark-lo').classList.remove('hide')
+    }
+})
 
 // slide script
 let slidesContainer = document.querySelector('.slides');
